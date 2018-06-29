c44c2c7d79a9901f9da0073ec4092739e1f68c20
Florent Beaurain <beaurain.florent@gmail.com>
2018-06-29 18:31:57 +0200

    sidekiq setup (#4)

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'mini_magick', '~> 4.8'
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'sidekiq'
+gem 'figaro'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module Recipy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.active_job.queue_adapter = :sidekiq
 
     config.generators do |g|
       g.assets false
