0d39703ddca2fa672d0fa5d10e9a4e7913eac2ef
Florent Beaurain <beaurain.florent@gmail.com>
2018-08-16 08:23:17 +0200

    chore: add CI (#75)

--- spec/controllers/cook/home_controller_spec.rb
+++ spec/controllers/cook/home_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Users::HomeController, type: :controller do
+RSpec.describe Cook::HomeController, type: :controller do
   describe "GET #index" do
-    before { get :index }
+    before { get :index, params: { locale: :fr } }
 
     it { should respond_with(:success) }
   end
--- spec/controllers/producer/home_controller_spec.rb
+++ spec/controllers/producer/home_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Producers::HomeController, type: :controller do
+RSpec.describe Producer::HomeController, type: :controller do
   describe "GET #index" do
-    before { get :index }
+    before { get :index, params: { locale: :fr } }
 
     it { should respond_with(:success) }
   end
