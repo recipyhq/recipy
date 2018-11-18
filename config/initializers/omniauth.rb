12ac1b2fe82c434eaf9cc68389db94c6b2cd9498
Florent Beaurain <beaurain.florent@gmail.com>
2018-11-18 16:15:50 +0100

    chore: initialize omniauth only in prodution environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+if Rails.env.production?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  end
 end
