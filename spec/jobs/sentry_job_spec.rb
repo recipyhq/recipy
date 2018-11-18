656cde40062fec40af5abfa91b6cc942a3b1b0cf
Florent Beaurain <beaurain.florent@gmail.com>
2018-11-18 18:51:11 +0100

    fix: correct tests for sentry_job

--- spec/jobs/sentry_job_spec.rb
+++ spec/jobs/sentry_job_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe SentryJob, type: :job do
   subject { described_class }
 
-  it { expect { subject.perform_later }.to have_enqueued_job.on_queue('default') }
+  it { expect { subject.perform_later }.to have_enqueued_job.on_queue('sentries') }
 end
