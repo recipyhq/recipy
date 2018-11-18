8359b58aec711340ff711b204842951380b9b15f
Florent Beaurain <beaurain.florent@gmail.com>
2018-11-18 16:27:42 +0100

    chore: use guard close instead

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-if Rails.env.production?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-  end
+return unless Rails.env.production?
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
 end
