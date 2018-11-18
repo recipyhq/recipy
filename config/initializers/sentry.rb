8c05393afb06b08ceef1201a584c05441dc8758d
Florent Beaurain <beaurain.florent@gmail.com>
2018-11-18 16:26:43 +0100

    chore: limits the number of sentries enqueued

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,13 @@
-if Rails.env.production?
-  Raven.configure do |config|
-    config.async = lambda { |event| SentryJob.perform_later(event) }
+return unless Rails.env.production?
+
+MAXIMUM_SENTRIES_ENQUEUED = 100
+
+Raven.configure do |config|
+  config.async = lambda do |event|
+    if Sidekiq::Queue.new('sentries').size <= MAXIMUM_SENTRIES_ENQUEUED
+      SentryJob.perform_later(event)
+    else
+      Rails.logger.warn "Too many sentries enqueued. We drop this one: #{event.inspect}"
+    end
   end
 end
