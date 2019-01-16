30512c7b0c9e90d42459a9e90b73c87d45cdafc7
Vincent Dusautoir <vdnet59@gmail.com>
2019-01-16 03:37:04 +0100

    Fix: Change class in css-class in mjml templates (#166)

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,11 @@
 class DeviseMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mails/devise'
+
+  def mail(headers)
+    super(headers) do |format|
+      format.mjml
+      format.text
+    end
+  end
 end
