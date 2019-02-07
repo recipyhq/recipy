baf86fb0befa15a79ff4cfee2cef39a1f9f60fb7
Robin Grattepanche <robin.grattepanche@epitech.eu>
2019-02-07 21:33:33 +0000

    fix: oauth ulrs (#170)
    * fix: oauth ulrs
    
    * fix: url facebook id
    
    * fix: url facebook callback
    
    * fix: navbar dropdown icon + trad
    
    * fix: checkbox front

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :confirmable, :recoverable, :registerable,
          :rememberable, :trackable, :timeoutable, :validatable, :lockable,
-         :omniauthable, omniauth_providers: %i(facebook twitter google_oauth2)
+         :omniauthable, omniauth_providers: %i(facebook google_oauth2)
 
   has_one_attached :avatar
 
@@ -23,12 +23,10 @@ class User < ActiveRecord::Base
 
   has_many :memberships, dependent: :destroy
 
-  class << self
-    def from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0, 20]
-      end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
-return unless Rails.env.production?
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+#   provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET']
+# end
