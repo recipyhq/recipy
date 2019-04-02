91bef2ef70abaa6cba9e1f84d48bc564c1b3d4d8
Vincent Dusautoir <vdnet59@gmail.com>
2019-04-02 13:25:50 -0400

    Feat: Add random recipe link (#214)

--- app/controllers/cook/home_controller.rb
+++ app/controllers/cook/home_controller.rb
@@ -2,7 +2,8 @@ module Cook
   class HomeController < Cook::BaseController
     def index
       skip_policy_scope
-      @recipes = Recipe.includes(:image_attachment => :blob).first(2)
+      @recipes = Recipe.includes(:image_attachment => :blob).first(4)
+      @random_recipe = Recipe.order("RANDOM()").first
     end
   end
 end
