ee25244fff6dc0afce4fb57a9e3ebd381ea0f24b
Vincent Dusautoir <ventinc@Vincents-MacBook-Pro.local>
2019-04-06 09:58:54 -0400

    Fix: Remove safety_assured

--- db/migrate/20190325010553_remove_notes_recipes.rb
+++ db/migrate/20190325010553_remove_notes_recipes.rb
@@ -1,5 +1,5 @@
 class RemoveNotesRecipes < ActiveRecord::Migration[5.2]
   def change
-    safety_assured { remove_column :recipes, :score, :integer }
+    remove_column :recipes, :score, :integer
   end
 end
