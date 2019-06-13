54ec32809c8a88e6744803e4d37c3d1c9fd39904
Quentin Baudet <quentin.baudet@epitech.eu>
2019-06-13 17:55:51 +0200

    fix: controller wrong include

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -8,7 +8,9 @@ class NotebooksController < InheritedResources::Base
     # @notebooks = Notebook.all
     @notebook_recipes_select = Recipe.all.map { |v| [v.title, v.id] }.to_a
     if user_signed_in?
-      @notebooks = Notebook.includes(:image_attachment => :blob).where(:user_id => current_user.id)
+      @notebooks = Notebook.includes(:image_attachment => :blob).where(
+        :user_id => current_user.id
+      ).order(:title)
     end
   end
 
