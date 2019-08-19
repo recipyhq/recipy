0c40806215f6e81cfe11c6582c9a1c13b1925197
PierreCooper <staszak.pierre@gmail.com>
2019-08-19 08:28:55 +0200

    newsletters and isProducer set to false (#300)

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -6,19 +6,27 @@ class Api::UserController < ApplicationController
     begin
       current_user = User.find(params[:user_id])
     rescue ActiveRecord::RecordNotFound
-      render :json => { Status: "KO", Cause: t("recipe.api.invalid_user_id") }.as_json,
+      render :json => {Status: "KO", Cause: t("recipe.api.invalid_user_id")}.as_json,
              status: :not_found
       return
     end
 
     render :json => {
-      firstname: current_user.first_name,
-      lastname: current_user.last_name,
-      email: current_user.email,
-      liked_producers: current_user.liked_producers,
-      followed_users: current_user.followed_users,
-      url: current_user.avatar.attached? ? rails_blob_url(current_user.avatar) : nil,
-    }.as_json
+      data: {
+        id: current_user.id,
+        email: current_user.email,
+        provider: current_user.provider,
+        first_name: current_user.first_name,
+        last_name: current_user.last_name,
+        uid: current_user.uid,
+        allow_password_change: current_user.allow_password_change,
+        newsletters: current_user.newsletters,
+        isProducer: current_user.isProducer,
+        bio: current_user.bio,
+        liked_producers: current_user.liked_producers,
+        followed_users: current_user.followed_users,
+        url: current_user.avatar.attached? ? rails_blob_url(current_user.avatar) : nil,
+      }.as_json}
   end
 
   def show_liked_producers
@@ -30,12 +38,12 @@ class Api::UserController < ApplicationController
       producer = User.find(params[:producer_id])
       current_user = User.find(params[:user_id])
     rescue ActiveRecord::RecordNotFound
-      render :json => { Status: "KO", Cause: t("recipe.api.invalid_user_id") }.as_json,
+      render :json => {Status: "KO", Cause: t("recipe.api.invalid_user_id")}.as_json,
              status: :not_found
       return
     end
     current_user.liked_producers << producer
-    render :json => { Status: "OK", Cause: t("users.api.follow_ok") }.as_json,
+    render :json => {Status: "OK", Cause: t("users.api.follow_ok")}.as_json,
            status: :ok
   end
 
@@ -44,12 +52,12 @@ class Api::UserController < ApplicationController
       producer = User.find(params[:producer_id])
       current_user = User.find(params[:user_id])
     rescue ActiveRecord::RecordNotFound
-      render :json => { Status: "KO", Cause: t("recipe.api.invalid_user_id") }.as_json,
+      render :json => {Status: "KO", Cause: t("recipe.api.invalid_user_id")}.as_json,
              status: :not_found
       return
     end
     current_user.liked_producers.destroy(producer)
-    render :json => { Status: "OK", Cause: t("users.api.follow_ko") }.as_json,
+    render :json => {Status: "OK", Cause: t("users.api.follow_ko")}.as_json,
            status: :ok
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_145838) do
+ActiveRecord::Schema.define(version: 2019_08_12_093248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -473,8 +473,8 @@ ActiveRecord::Schema.define(version: 2019_07_14_145838) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "newsletters"
-    t.boolean "isProducer"
+    t.boolean "newsletters", default: false, null: false
+    t.boolean "isProducer", default: false, null: false
     t.text "bio"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
