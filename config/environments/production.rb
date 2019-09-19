3a0dd9d5ae8391a77ffddb21c257ea0ed2d7ac96
Vincent Dusautoir <ventinc@Vincents-MacBook-Pro.local>
2019-09-19 14:45:45 +0200

    Fix: change url options in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "recipy_#{Rails.env}"
 
+  config.action_mailer.default_url_options = {:host => 'recipy.fr', host: 'https' } 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
