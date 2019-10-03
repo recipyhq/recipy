8005ffbf9f26501b1997afb9b36908952cf36302
Thornolf <quentin.baudet@epitech.eu>
2019-10-03 21:38:41 +0200

    Feature/popular recipe (#336)
    * fix: truncate on description on homescreen
    
    * fix: order desc on popular recipe
    
    * fix: truncate the text

--- app/controllers/cook/home_controller.rb
+++ app/controllers/cook/home_controller.rb
@@ -2,7 +2,7 @@ module Cook
   class HomeController < Cook::BaseController
     def index
       skip_policy_scope
-      @recipes = Recipe.order(:created_at).includes(:image_attachment => :blob).first(4)
+      @recipes = Recipe.order(view: :desc).includes(:image_attachment => :blob).first(4)
       @random_recipe = Recipe.order("RANDOM()").first
       if @random_recipe.nil?
         puts "Les recettes ne sont pas disponibles"
